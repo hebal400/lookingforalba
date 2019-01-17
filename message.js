4d3015bc3548c9c58b146c1af9ce82290a075772
aerain <j40f893@gmail.com>
2019-01-17 18:14:42 +0900

    탭, 팝업, iframe 간 메시지 교환

--- message.js
+++ message.js
@@ -1,9 +1,28 @@
-window.onload = () => {
-    let childFrame = document.getElementById('wrap');
 
-    let origin = window.origin,
-        parsedData = '테스트 메시지 날립니다.';
 
-    let dataObject = { origin, parsedData };
-    childFrame.contentWindow.postMessage(dataObject, '*');
+function whenLoaded(event) {
+    window.addEventListener('message', (event) => {
+        chrome.tabs.query({
+            active: true,
+            currentWindow: true
+        }, tabs => {
+            chrome.tabs.sendMessage(
+                tabs[0].id,
+                { send: "requestDataFromDOM" },
+                callbackAfterLoad
+            )
+        })
+    }, false);
+    
 }
+
+function callbackAfterLoad(parsedData) {
+    let childFrame = document.getElementById('wrap');
+    let origin = window.origin;
+    let dataObject = { origin, parsedData };
+
+    childFrame.contentWindow.postMessage(dataObject, '*')
+    console.log('test 메시지')
+}
+
+window.addEventListener('DOMContentLoaded', whenLoaded);
--- parse.js
+++ parse.js
@@ -1,1 +1,6 @@
-console.log('어디에서 수행되는가?')
+chrome.runtime.onMessage.addListener((msg, sender, response) => {
+    if(msg.send === 'requestDataFromDOM') {
+        let send = "추후 보내질 데이터";
+        response(send);
+    }
+})
